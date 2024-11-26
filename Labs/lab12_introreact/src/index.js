4b5157f691ff0ea82c08d3a9738c158548c4ef90
aseelrahman <132919956+aseelrahman@users.noreply.github.com>
2024-11-26 16:47:20 +0000

    lab12 updated

--- Labs/lab12_introreact/src/index.js
+++ Labs/lab12_introreact/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import landscape from './images/landscape.jpg'
+import './index.css'
 
 // content in the app
 const App = function(){
+  // creating a JSX variabel
+  const anymsg = "I am a text constant variable"
   return(
     <>
     <h1 style={{color:'magenta', textAlign:'center'}}>Welcome to React JS</h1>
-    <h2 style={{fontFamily:'algerian'}}>Aseel Rahman</h2>
-    <figure style={{border:'inset olive 20px'}}>
+    <h2 style={{fontFamily:'algerian'}} className='subtitle'>Aseel Rahman</h2>
+    <figure style={{border:'inset olive 20px', width:'60%', margin:'auto'}}>
       <img src={landscape} style={{width:'100%', display:'block'}}/>
     </figure>
+    <h2 style={{padding:'2em'}}>{anymsg}</h2>
     </>
   )
 }
