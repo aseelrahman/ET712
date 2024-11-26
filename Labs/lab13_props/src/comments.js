5a66f675e3244374bf2a1ed8da3cadb5de02fef4
aseelrahman <132919956+aseelrahman@users.noreply.github.com>
2024-11-26 17:30:34 +0000

    props lab

---
+++ Labs/lab13_props/src/comments.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './index';
+
+
+const User = function(props){
+    return(
+        <>
+            <section className="card">
+                <a><img src={props.image}/></a>
+                <div className="content">
+                    <a className="author">{props.name}</a>
+                </div>
+                <div className="metadata">
+                    <p>Posted on <span className="date">{props.date}</span></p>
+                </div>
+                <div className="comment">
+                    <p>{props.msg}</p>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default User;
