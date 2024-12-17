64294662b2b82d25827f8ad465e68116f10eedbd
aseelrahman <132919956+aseelrahman@users.noreply.github.com>
2024-12-17 18:26:11 +0000

    project 2 final draft

--- Projects/project_02/src/components/modalwindow.js
+++ Projects/project_02/src/components/modalwindow.js
@@ -3,27 +3,24 @@ import "../App.css";
 
 const Modalwindow = function ({ title, image, description, onClose }) {
     return (
-        <>
-            {/* Modal window */}
-            <section className="modalwindow">
-                <div className="modalcontent">
-                    <header className="modalheader">
-                        <p>{title}</p> {/* Title dynamically passed */}
-                        <p className="closemodal" onClick={onClose}>
-                            &#x58; {/* Close button */}
-                        </p>
-                    </header>
-                    <main className="modalbody">
-                        <img
-                            src={image}
-                            alt={title}
-                            className="modalimage"
-                        />
-                        <p className="modaldescription">{description}</p>
-                    </main>
-                </div>
-            </section>
-        </>
+        <section className="modalwindow">
+            <div className="modalcontent">
+                <header className="modalheader">
+                    <p>{title}</p> {/* Title */}
+                    <span className="closemodal" onClick={onClose}>
+                        &#x58;
+                    </span>
+                </header>
+                <main className="modalbody">
+                    <img
+                        src={image}
+                        alt={title}
+                        className="modalimage"
+                    />
+                    <p className="modaldescription">{description}</p>
+                </main>
+            </div>
+        </section>
     );
 };
 
